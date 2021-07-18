68c5e3a69b1c64b4c184e15b5a756bf5fa3b08df
xio810 <boi4806@naver.com>
2021-07-18 17:15:16 +0900

    count 조절 액션구현

--- src/main/java/com/xio/exam/community/controller/UsrHomeController.java
+++ src/main/java/com/xio/exam/community/controller/UsrHomeController.java
@@ -12,36 +12,17 @@ public class UsrHomeController {
 		count = -1;
 	}
 
-	@RequestMapping("/usr/home/main")
-	@ResponseBody
-	public String showMain() {
-		return "안녕하세요/";
-	}
-
-	@RequestMapping("/usr/home/main2")
-	@ResponseBody
-	public String showMain2() {
-		return "반갑습니다/";
-	}
-
-	@RequestMapping("/usr/home/main3")
-	@ResponseBody
-	public String showMain3() {
-		return "또 만나요/";
-	}
-
-	@RequestMapping("/usr/home/main4")
+	@RequestMapping("/usr/home/getCount")
 	@ResponseBody
 	public int showMain4() {
-		count++;
 		return count;
 	}
 
-	@RequestMapping("/usr/home/main5")
+	@RequestMapping("/usr/home/doSetCount")
 	@ResponseBody
-	public String showMain5() {
-		count = -1;
-		return "count값 초기화";
+	public String showMain5(int count) {
+		this.count = count;
+		return "count값이 " +this.count +"로 초기화 되었습니다.";
 	}
 
 }
