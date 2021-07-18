9d2ad082d6290ff740806213406ff23fa79adc58
xio810 <boi4806@naver.com>
2021-07-18 17:10:12 +0900

    count 값 연습

--- src/main/java/com/xio/exam/community/controller/UsrHomeController.java
+++ src/main/java/com/xio/exam/community/controller/UsrHomeController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
+	private int count;
+
+	public UsrHomeController() {
+		count = -1;
+	}
+
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
@@ -17,11 +23,25 @@ public class UsrHomeController {
 	public String showMain2() {
 		return "반갑습니다/";
 	}
-	
+
 	@RequestMapping("/usr/home/main3")
 	@ResponseBody
 	public String showMain3() {
 		return "또 만나요/";
 	}
-	
+
+	@RequestMapping("/usr/home/main4")
+	@ResponseBody
+	public int showMain4() {
+		count++;
+		return count;
+	}
+
+	@RequestMapping("/usr/home/main5")
+	@ResponseBody
+	public String showMain5() {
+		count = -1;
+		return "count값 초기화";
+	}
+
 }
