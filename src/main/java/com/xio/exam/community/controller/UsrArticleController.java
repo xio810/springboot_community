7de228b12663d0a593232cbbfc1140a25f175b9a
xio810 <boi4806@naver.com>
2021-07-24 16:23:01 +0900

    게시물 삭제기능 수정

--- src/main/java/com/xio/exam/community/controller/UsrArticleController.java
+++ src/main/java/com/xio/exam/community/controller/UsrArticleController.java
@@ -12,94 +12,82 @@ import com.xio.exam.community.vo.Article;
 @Controller
 public class UsrArticleController {
 	// 인스턴스 변수 시작
-	private int articlesLastId;
-	private List<Article> articles;
-	// 인스턴스 변수 끝
+		private int articlesLastId;
+		private List<Article> articles;
+		// 인스턴스 변수 끝
 
-	// 생성자
-	public UsrArticleController() {
-		articlesLastId = 0;
-		articles = new ArrayList<>();
+		// 생성자
+		public UsrArticleController() {
+			articlesLastId = 0;
+			articles = new ArrayList<>();
 
-		makeTestData();
-	}
-
-	// 서비스 메서드 시작
-	private void makeTestData() {
-
-		for (int i = 1; i <= 10; i++) {
-
-			String title = "제목 " + i;
-			String body = "내용 " + i;
-
-			writeArticle(title, body);
+			makeTestData();
 		}
 
-	}
-
-	private Article writeArticle(String title, String body) {
-		int id = articlesLastId + 1;
-
-		Article article = new Article(id, title, body);
-		articles.add(article);
-		articlesLastId = id;
-		return article;
-	}
-
-	private Article getArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
+		// 서비스 메서드 시작
+		private void makeTestData() {
+			for (int i = 1; i <= 10; i++) {
+				String title = "제목 " + i;
+				String body = "내용 " + i;
+				
+				writeArticle(title, body);
 			}
 		}
-		return null;
-	}
+		
+		public Article writeArticle(String title, String body) {
+			int id = articlesLastId + 1;
+			Article article = new Article(id, title, body);
 
-	private void deleteArticle(int id) {
+			articles.add(article);
+			articlesLastId = id;
+			
+			return article;
+		}
+		
+		private Article getArticle(int id) {
+			for ( Article article : articles ) {
+				if ( article.getId() == id ) {
+					return article;
+				}
+			}
+			
+			return null;
+		}
+		
+		private void deleteArticle(int id) {
+			Article article = getArticle(id);
+			
+			articles.remove(article);
+		}
+		// 서비스 메서드 끝
 
-		Article article = getArticle(id);
+		// 액션 메서드 시작
+		@RequestMapping("/usr/article/doAdd")
+		@ResponseBody
+		public Article doAdd(String title, String body) {
+			Article article = writeArticle(title, body);
 
-		articles.remove(id);
-
-	}
-	// 서비스 메서드 끝
-
-	// 액션 메서드 시작
-	@RequestMapping("/usr/article/doAdd")
-	@ResponseBody
-	public Article doAdd(String title, String body) {
-
-		Article article = writeArticle(title, body);
-
-		articles.add(article);
-
-		return article;
-	}
-
-	@RequestMapping("/usr/article/doDelete")
-	@ResponseBody
-	public String doDelete(int id) {
-		Article article = getArticle(id);
-
-		if (article == null) {
-			return id + " 번 게시물이 존재하지 않습니다.";
+			return article;
 		}
 
-		deleteArticle(id);
+		@RequestMapping("/usr/article/getArticles")
+		@ResponseBody
+		public List<Article> getArticles() {
+			return articles;
+		}
 
-		return id + " 번 게시물을 삭제하였습니다.";
+		@RequestMapping("/usr/article/doDelete")
+		@ResponseBody
+		public String doDelete(int id) {
+			Article article = getArticle(id);
+			
+			if ( article == null ) {
+				return id + "번 게시물이 존재하지 않습니다.";			
+			}
+			
+			deleteArticle(id);
+			
+			return id + "번 게시물을 삭제하였습니다.";
+		}
+		// 액션 메서드 끝
 	}
-
-	@RequestMapping("/usr/article/doModify")
-	@ResponseBody
-	public Article doModify(int id, String title, String body) {
-
-	}
-
-	@RequestMapping("/usr/article/getArticles")
-	@ResponseBody
-	public List<Article> getArticles() {
-		return articles;
-	}
-	// 액션 메서드 끝
-}
