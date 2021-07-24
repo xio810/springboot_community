38605602750fe291c569b05a60278d7aaa60928b
xio810 <boi4806@naver.com>
2021-07-24 16:00:26 +0900

    testData 생성

--- src/main/java/com/xio/exam/community/controller/UsrArticleController.java
+++ src/main/java/com/xio/exam/community/controller/UsrArticleController.java
@@ -18,18 +18,46 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		articlesLastId = 0;
 		articles = new ArrayList<>();
+
+		makeTestData();
+	}
+
+	private void makeTestData() {
+
+		for (int i = 1; i <= 10; i++) {
+			int id = articlesLastId + 1;
+			String title = "제목 " + i;
+			String body = "내용 " + i;
+			
+			Article article = new Article(id,title,body);
+			articles.add(article);
+			articlesLastId = id;
+		}
+
 	}
 
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
 		int id = articlesLastId + 1;
+		articlesLastId = id;
 		Article article = new Article(id, title, body);
 
 		articles.add(article);
-		articlesLastId = id;
 
 		return article;
+	}
+
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public Article doModify(int id, String title, String body) {
+
+	}
+
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public Article doDelete(int id) {
+
 	}
 
 	@RequestMapping("/usr/article/getArticles")
--- src/main/java/com/xio/exam/community/controller/UsrHomeController.java
+++ src/main/java/com/xio/exam/community/controller/UsrHomeController.java
@@ -1,28 +1,35 @@
 package com.xio.exam.community.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.xio.exam.community.vo.Article;
+
 @Controller
 public class UsrHomeController {
-	private int count;
-
-	public UsrHomeController() {
-		count = -1;
-	}
-
-	@RequestMapping("/usr/home/getCount")
+	
+	@RequestMapping("/usr/home/getArticle")
 	@ResponseBody
-	public int showMain4() {
-		return count;
+	public Article getArticle() {
+		Article article = new Article(1, "1", "1");
+		Article article2 = new Article(2, "2", "2");
+		
+		return article2;
 	}
-
-	@RequestMapping("/usr/home/doSetCount")
+	
+	@RequestMapping("/usr/home/getArticles")
 	@ResponseBody
-	public String showMain5(int count) {
-		this.count = count;
-		return "count값이 " +this.count +"로 초기화 되었습니다.";
+	public List<Article> getArticles() {
+		Article article1 = new Article(1, "1", "1");
+		Article article2 = new Article(2, "2", "2");
+		
+		List<Article> list = new ArrayList<>();
+		list.add(article1);
+		
+		return list;
 	}
-
 }
