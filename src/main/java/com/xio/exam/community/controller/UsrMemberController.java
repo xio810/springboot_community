3233a95d3bdf4bcf44330dc5d4dd79fae1a3878c
xio810 <boi4806@naver.com>
2021-08-01 03:20:28 +0900

    회원가입 시, 입력데이터 유효성 체크

--- src/main/java/com/xio/exam/community/controller/UsrMemberController.java
+++ src/main/java/com/xio/exam/community/controller/UsrMemberController.java
@@ -19,12 +19,33 @@ public class UsrMemberController {
 	@ResponseBody
 	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
 			String email) {
+		//
+		if (loginId == null) {
+			return "login id를 입력해 주세요.";
+		}
+		if (loginPw == null) {
+			return "login pw를 입력해 주세요.";
+		}
+		if (name == null) {
+			return "name을 입력해 주세요.";
+		}
+		if (nickname == null) {
+			return "nickname을 입력해 주세요.";
+		}
+		if (cellphoneNo == null) {
+			return "cellphoneNo를 입력해 주세요.";
+		}
+		if (email == null) {
+			return "email을 입력해 주세요.";
+		}
+		//
+
 		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-		
-		if(id == -1) {
+
+		if (id == -1) {
 			return "해당 아이디는 사용중 입니다.";
 		}
-		
+
 		Member member = memberService.getMemberById(id);
 
 		return member;
