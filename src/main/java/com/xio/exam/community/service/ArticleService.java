33cd338b0c874490ea84371ff5b598650cc09dd8
xio810 <boi4806@naver.com>
2021-08-01 17:15:08 +0900

    doAdd 변경

--- src/main/java/com/xio/exam/community/service/ArticleService.java
+++ src/main/java/com/xio/exam/community/service/ArticleService.java
@@ -5,8 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.xio.exam.community.repository.ArticleRepository;
+import com.xio.exam.community.util.Ut;
 import com.xio.exam.community.vo.Article;
-import com.xio.exam.community.vo.Member;
+import com.xio.exam.community.vo.ResultData;
 
 @Service
 public class ArticleService {
@@ -16,10 +17,12 @@ private ArticleRepository articleRepository;
 		this.articleRepository = articleRepository;
 	}
 
-	public int writeArticle(String title, String body) {
-		articleRepository.writeArticle(title, body);
-		return articleRepository.getLastInsertId();
-	}
+	public ResultData writeArticle(String title, String body) {
+ 		articleRepository.writeArticle(title, body);
+ 		int id = articleRepository.getLastInsertId();
+
+ 		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다.", id), id);
+ 	}
 
 	public List<Article> getArticles() {
 		return articleRepository.getArticles();
