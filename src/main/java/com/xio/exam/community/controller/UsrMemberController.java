92005c8dc9a710e4dcc3ef8bf31b14efbc8fb5b2
xio810 <boi4806@naver.com>
2021-08-02 02:50:47 +0900

    doJoin 변경

--- src/main/java/com/xio/exam/community/controller/UsrMemberController.java
+++ src/main/java/com/xio/exam/community/controller/UsrMemberController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.xio.exam.community.service.MemberService;
 import com.xio.exam.community.util.Ut;
 import com.xio.exam.community.vo.Member;
+import com.xio.exam.community.vo.ResultData;
 
 @Controller
 public class UsrMemberController {
@@ -18,41 +19,37 @@ public class UsrMemberController {
 
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+	public ResultData doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
 			String email) {
 		//
 		if (Ut.empty(loginId)) {
-			return "login id를 입력해 주세요.";
+			return ResultData.from("F-1", "loginId를 입력해 주세요");
 		}
 		if (Ut.empty(loginPw)) {
-			return "login pw를 입력해 주세요.";
+			return ResultData.from("F-2", "loginPw를 입력해 주세요");
 		}
 		if (Ut.empty(name)) {
-			return "name을 입력해 주세요.";
+			return ResultData.from("F-3", "name을 입력해 주세요");
 		}
 		if (Ut.empty(nickname)) {
-			return "nickname을 입력해 주세요.";
+			return ResultData.from("F-4", "nickname을 입력해 주세요");
 		}
 		if (Ut.empty(cellphoneNo)) {
-			return "cellphoneNo를 입력해 주세요.";
+			return ResultData.from("F-5", "cellphoneNo를 입력해 주세요");
 		}
 		if (Ut.empty(email)) {
-			return "email을 입력해 주세요.";
+			return ResultData.from("F-6", "e-mail을 입력해 주세요");
 		}
 		//
 
-		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-
-		if (id == -1) {
-			return Ut.f("해당 아이디(%s)는 사용중 입니다.", loginId);
+		ResultData joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		
+		if (joinRd.isFail()) {
+			return joinRd;
 		}
 
-		if (id == -2) {
-			return Ut.f("해당 이름(%s)과 이메일(%s)은 사용중 입니다.", name, email);
-		}
-
-		Member member = memberService.getMemberById(id);
-
-		return member;
+		
+		Member member = memberService.getMemberById((int) joinRd.getData1());
+		return ResultData.newData(joinRd, member);
 	}
 }
--- src/main/java/com/xio/exam/community/service/MemberService.java
+++ src/main/java/com/xio/exam/community/service/MemberService.java
@@ -3,7 +3,9 @@ package com.xio.exam.community.service;
 import org.springframework.stereotype.Service;
 
 import com.xio.exam.community.repository.MemberRepository;
+import com.xio.exam.community.util.Ut;
 import com.xio.exam.community.vo.Member;
+import com.xio.exam.community.vo.ResultData;
 
 @Service
 public class MemberService {
@@ -14,23 +16,25 @@ public class MemberService {
 		this.memberRepository = memberRepository;
 	}
 
-	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+	public ResultData join(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+			String email) {
 		// 로그인 아이디 중복체크
 		Member oldMember = getMemberByLoginId(loginId);
 
 		if (oldMember != null) {
-			return -1;
+			return ResultData.from("F-7", Ut.f("해당 로그인 아이디(%s)는 이미 사용중 입니다.", loginId));
 		}
 		// 로그인 시 이름, 이메일 중복체크
 		oldMember = getMemberByNameAndEmail(name, email);
 
 		if (oldMember != null) {
-			return -2;
+			return ResultData.from("F-8", Ut.f("해당 이름(%s)과 이메일(%s)은 이미 사용중 입니다.", name, email));
 		}
 
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 
-		return memberRepository.getLastInsertId();
+		int id = memberRepository.getLastInsertId();
+		return ResultData.from("S-1", "회원가입이 완료되었습니다.", id);
 	}
 
 	private Member getMemberByNameAndEmail(String name, String email) {
