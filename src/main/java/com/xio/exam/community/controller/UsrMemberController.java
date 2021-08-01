e88648d618a2df4ee73117c9c8349baae39c7310
xio810 <boi4806@naver.com>
2021-08-02 04:47:06 +0900

    doLogin 구현

--- src/main/java/com/xio/exam/community/controller/UsrMemberController.java
+++ src/main/java/com/xio/exam/community/controller/UsrMemberController.java
@@ -1,5 +1,7 @@
 package com.xio.exam.community.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -43,13 +45,47 @@ public class UsrMemberController {
 		//
 
 		ResultData<Integer> joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-		
+
 		if (joinRd.isFail()) {
-			return (ResultData)joinRd;
+			return (ResultData) joinRd;
 		}
 
-		
 		Member member = memberService.getMemberById(joinRd.getData1());
 		return ResultData.newData(joinRd, member);
+	}
+
+	@RequestMapping("/usr/member/doLogin")
+	@ResponseBody
+	public ResultData doLogin(HttpSession httpSession, String loginId, String loginPw) {
+		boolean isLogined = false;
+
+		if (httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+		}
+		
+		if(isLogined) {
+			return ResultData.from("F-5", "이미 로그인 되었습니다.");
+		}
+		
+		if (Ut.empty(loginId)) {
+			return ResultData.from("F-1", "loginId를 입력해 주세요.");
+		}
+
+		if (Ut.empty(loginPw)) {
+			return ResultData.from("F-2", "loginPw를 입력해 주세요.");
+		}
+
+		Member member = memberService.getMemberByLoginId(loginId);
+
+		if (member == null) {
+			return ResultData.from("F-3", "존재하지 않는 아이디 입니다.");
+		}
+		if (member.getLoginPw().equals(loginPw) == false) {
+			return ResultData.from("F-4", "비밀번호가 일치하지 않습니다.");
+		}
+		
+		httpSession.setAttribute("loginedMemberId", member.getId());
+
+		return ResultData.from("S-1", Ut.f("%S님 로그인 되었습니다.", member.getNickname()));
 	}
 }
