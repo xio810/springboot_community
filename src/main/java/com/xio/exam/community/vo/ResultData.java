068082a381fdb777f909bd8c4c0b118e62c68e96
xio810 <boi4806@naver.com>
2021-08-01 16:56:39 +0900

    resultData 도입

---
+++ src/main/java/com/xio/exam/community/vo/ResultData.java
@@ -0,0 +1,38 @@
+package com.xio.exam.community.vo;
+
+import lombok.Getter;
+
+public class ResultData {
+	@Getter
+	private String resultCode;
+	@Getter
+	private String msg;
+	@Getter
+	private Object data1;
+
+	public ResultData() {
+
+	}
+
+	public static ResultData from(String resultCode, String msg) {
+		return from(resultCode, msg, null);
+	}
+
+	public static ResultData from(String resultCode, String msg, Object data1) {
+		ResultData rd = new ResultData();
+
+		rd.resultCode = resultCode;
+		rd.msg = msg;
+		rd.data1 = data1;
+
+		return rd;
+	}
+
+	public boolean isSuccess() {
+		return resultCode.startsWith("s-1");
+	}
+
+	public boolean isFail() {
+		return isSuccess() == false;
+	}
+}
