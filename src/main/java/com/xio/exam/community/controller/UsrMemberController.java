da4855ca4a74b731143d81c875ab9414bbf4c85e
xio810 <boi4806@naver.com>
2021-08-02 04:14:47 +0900

    UsrMemberController, MemberService 에 ResultData 제너릭 적용

--- src/main/java/com/xio/exam/community/controller/UsrMemberController.java
+++ src/main/java/com/xio/exam/community/controller/UsrMemberController.java
@@ -19,7 +19,7 @@ public class UsrMemberController {
 
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public ResultData doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+	public ResultData<Member> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
 			String email) {
 		//
 		if (Ut.empty(loginId)) {
@@ -42,14 +42,14 @@ public class UsrMemberController {
 		}
 		//
 
-		ResultData joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		ResultData<Integer> joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 		
 		if (joinRd.isFail()) {
-			return joinRd;
+			return (ResultData)joinRd;
 		}
 
 		
-		Member member = memberService.getMemberById((int) joinRd.getData1());
+		Member member = memberService.getMemberById(joinRd.getData1());
 		return ResultData.newData(joinRd, member);
 	}
 }
