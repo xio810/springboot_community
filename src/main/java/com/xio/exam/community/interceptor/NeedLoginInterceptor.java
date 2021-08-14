587faa541973985246e87c08148ffdb31a98dfc4
xio810 <boi4806@naver.com>
2021-08-14 16:40:51 +0900

    NeedLogin인터셉터가 여러 액션들을 대신해서 자격없는 요청들을 앞선에서 거름

--- src/main/java/com/xio/exam/community/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/xio/exam/community/interceptor/NeedLoginInterceptor.java
@@ -6,11 +6,19 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.xio.exam.community.vo.Rq;
+
 @Component
 public class NeedLoginInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-		System.out.println("로그인 필요!!");
+		Rq rq = (Rq) req.getAttribute("rq");
+
+ 		if (!rq.isLogined()) {
+ 			rq.printHistoryBackJs("로그인 후 이용해주세요.");
+ 			return false;
+ 		}
+		
 
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
--- src/main/java/com/xio/exam/community/vo/Rq.java
+++ src/main/java/com/xio/exam/community/vo/Rq.java
@@ -1,7 +1,12 @@
 package com.xio.exam.community.vo;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import com.xio.exam.community.util.Ut;
 
 import lombok.Getter;
 
@@ -10,8 +15,15 @@ public class Rq {
 	private boolean isLogined;
 	@Getter
 	private int loginedMemberId;
-
-	public Rq(HttpServletRequest req) {
+	
+	private HttpServletRequest req;
+	private HttpServletResponse resp; 
+	
+	public Rq(HttpServletRequest req, HttpServletResponse resp) {
+		
+		this.req = req;
+		this.resp = resp;
+		
 		HttpSession httpSession = req.getSession();
 		boolean isLogined = false;
 		int loginedMemberId = 0;
@@ -24,4 +36,30 @@ public class Rq {
 		this.isLogined = isLogined;
 		this.loginedMemberId = loginedMemberId;
 	}
+	
+	public void printHistoryBackJs(String msg) {
+ 		resp.setContentType("text/html; charset=UTF-8");
+
+ 		println("<script>");
+
+ 		if (!Ut.empty(msg)) {
+ 			println("alert('" + msg + "');");
+ 		}
+
+ 		println("history.back();");
+
+ 		println("</script>");
+ 	}
+
+ 	public void print(String str) {
+ 		try {
+ 			resp.getWriter().append(str);
+ 		} catch (IOException e) {
+ 			e.printStackTrace();
+ 		}
+ 	}
+
+ 	public void println(String str) {
+ 		print(str + "\n");
+ 	}
 }
