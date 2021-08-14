889928396a6660d0bc40c410b83783bdc98ed65c
xio810 <boi4806@naver.com>
2021-08-14 16:30:46 +0900

    Rq객체를 BeforeAction 인터셉터에서 대표로 한번만 만들기

--- src/main/java/com/xio/exam/community/config/MyWebMvcConfigurer.java
+++ src/main/java/com/xio/exam/community/config/MyWebMvcConfigurer.java
@@ -1,4 +1,4 @@
-package com.xio.exam.community;
+package com.xio.exam.community.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +10,7 @@ import com.xio.exam.community.interceptor.NeedLoginInterceptor;
 
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
-	// beforeActionInterceptor 인터셉터 불러오기
+	// beforeActionInterceptor 인터셉터 불러오기.
 	@Autowired
 	BeforeActionInterceptor beforeActionInterceptor;
 	@Autowired
