552dcfa0c68d100f6333ca4c621b85e16f08c950
xio810 <boi4806@naver.com>
2021-08-29 03:14:06 +0900

    시간출력관련 함수 만들어서 사용

--- src/main/java/com/xio/exam/community/vo/Article.java
+++ src/main/java/com/xio/exam/community/vo/Article.java
@@ -15,16 +15,24 @@ public class Article {
 	private String title;
 	private String body;
 	private int hitCount;
-	
+
 	private String extra__writerName;
 	private boolean extra__actorCanDelete;
 	private boolean extra__actorCanModify;
-	
-	public String getRegDateForPrint() {
- 		return regDate.substring(2, 16);
- 	}
 
- 	public String getUpdateDateForPrint() {
- 		return updateDate.substring(2, 16);
- 	}
+	public String getForPrintType1RegDate() {
+		return regDate.substring(2, 16).replace(" ", "<br>");
+	}
+
+	public String getForPrintType1UpdateDate() {
+		return regDate.substring(2, 16).replace(" ", "<br>");
+	}
+
+	public String getForPrintType2RegDate() {
+		return regDate.substring(2, 16);
+	}
+
+	public String getForPrintType2UpdateDate() {
+		return updateDate.substring(2, 16);
+	}
 }
