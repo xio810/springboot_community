1a670846d17ff344411e8a9931e02847f799e02d
xio810 <boi4806@naver.com>
2021-09-01 20:11:55 +0900

    리액션포인트서비스, 리액션포인트리포지터리 도입

--- src/main/java/com/xio/exam/community/controller/UsrArticleController.java
+++ src/main/java/com/xio/exam/community/controller/UsrArticleController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.xio.exam.community.service.ArticleService;
 import com.xio.exam.community.service.BoardService;
+import com.xio.exam.community.service.ReactionPointService;
 import com.xio.exam.community.util.Ut;
 import com.xio.exam.community.vo.Article;
 import com.xio.exam.community.vo.Board;
@@ -22,11 +23,14 @@ import com.xio.exam.community.vo.Rq;
 public class UsrArticleController {
 	private ArticleService articleService;
 	private BoardService boardService;
+	private ReactionPointService reactionPointService;
 	private Rq rq;
 
-	public UsrArticleController(ArticleService articleService, BoardService boardService, Rq rq) {
+	public UsrArticleController(ArticleService articleService, BoardService boardService,
+			ReactionPointService reactionPointService, Rq rq) {
 		this.articleService = articleService;
 		this.boardService = boardService;
+		this.reactionPointService = reactionPointService;
 		this.rq = rq;
 	}
 
@@ -66,8 +70,9 @@ public class UsrArticleController {
 
 		model.addAttribute("article", article);
 
-		boolean actorCanMakeReactionPoint = articleService.actorCanMakeReactionPoint(rq.getLoginedMemberId(), id);
-		
+		boolean actorCanMakeReactionPoint = reactionPointService.actorCanMakeReactionPoint(rq.getLoginedMemberId(),
+				"article", id);
+
 		model.addAttribute("actorCanMakeReactionPoint", actorCanMakeReactionPoint);
 
 		return "usr/article/detail";
---
+++ src/main/java/com/xio/exam/community/service/ReactionPointService.java
@@ -0,0 +1,25 @@
+package com.xio.exam.community.service;
+
+import org.springframework.stereotype.Service;
+
+import com.xio.exam.community.repository.ReactionPointRepository;
+
+@Service
+public class ReactionPointService {
+
+	private ReactionPointRepository reactionPointRepository;
+
+	public ReactionPointService(ReactionPointRepository reactionPointRepository) {
+		this.reactionPointRepository = reactionPointRepository;
+	}
+
+	public boolean actorCanMakeReactionPoint(int actorId, String relTypeCode, int relId) {
+
+		if (actorId == 0) {
+			return false;
+		}
+		
+		return reactionPointRepository.getSumReactionPointByMemberId(relTypeCode, relId, actorId) == 0;
+	}
+
+}
