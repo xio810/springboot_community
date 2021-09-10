34b62cf914b52376ea34d6b8044c74b58acd7d75
xio810 <boi4806@naver.com>
2021-09-10 20:12:26 +0900

    회원정보 수정 창

--- src/main/java/com/xio/exam/community/controller/UsrMemberController.java
+++ src/main/java/com/xio/exam/community/controller/UsrMemberController.java
@@ -129,4 +129,9 @@ public class UsrMemberController {
 		return rq.jsReplace("", replaceUri);
 	}
 
+	@RequestMapping("/usr/member/modify")
+	public String showModify() {
+		return "usr/member/modify";
+	}
+
 }
